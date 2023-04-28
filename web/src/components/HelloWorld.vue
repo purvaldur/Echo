aab6acd890ac7e38beda7ab442382ddc3c3191a9
Thorvald Kjartansson <thk@netic.dk>
2023-04-28 16:22:44 +0200

    Small visual changes

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,13 +1,18 @@
 <script setup>
+import { ref } from 'vue'
 import { socket } from '../modules/socket.js'
 
-const song = ''
+let song = ref('')
+function play() {
+    socket.emit('play', song.value)
+    song.value = ''
+}
 </script>
 
 <template>
-  <h1>ECHO</h1>
-  <div>
-    <input type="text" v-model=song>
-    <button type="button" @click="socket.emit('play', song)">PLAY</button>
-  </div>
+    <h1>ECHO</h1>
+    <div>
+        <input type="text" @keyup.enter="play()" v-model=song>
+        <button type="button" @click="play()">PLAY</button>
+    </div>
 </template>
