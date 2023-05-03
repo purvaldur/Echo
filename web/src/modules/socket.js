e4b3a731a75b4a2f3891043c111c4ab05aacc834
Thorvald Kjartansson <thk@netic.dk>
2023-05-03 11:39:04 +0200

    Use env vars pass 3

--- web/src/modules/socket.js
+++ web/src/modules/socket.js
@@ -1,3 +1,3 @@
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
 
-export const socket = io(`ws://${import.meta.env.WSS_SERVER}:${import.meta.env.WSS_PORT}`)
+export const socket = io(`ws://${import.meta.env.WEB_WSS_SERVER}:${import.meta.env.WEB_WSS_PORT}`)
