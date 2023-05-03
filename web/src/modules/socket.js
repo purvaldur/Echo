ded70a68bd48381dc0046e2fdb4599d7e36dddbd
Thorvald Kjartansson <thk@netic.dk>
2023-05-03 11:30:52 +0200

    use env vars for websocket server config

--- web/src/modules/socket.js
+++ web/src/modules/socket.js
@@ -1,3 +1,3 @@
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
 
-export const socket = io("ws://localhost:3001")
+export const socket = io(`ws://${process.env.WSS_SERVER}:${process.env.WSS_PORT}`)
