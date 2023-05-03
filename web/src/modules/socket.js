83800e07f61ed5d8746ec84d214faaf8a96dd572
Thorvald Kjartansson <thk@netic.dk>
2023-05-03 11:33:07 +0200

    Import process from process

--- web/src/modules/socket.js
+++ web/src/modules/socket.js
@@ -1,3 +1,4 @@
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js"
+import { process } from "process";
 
 export const socket = io(`ws://${process.env.WSS_SERVER}:${process.env.WSS_PORT}`)
